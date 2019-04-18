4b8c02c17d4f33b1e4a017232ba3046c804475cb
Adella <adellakenn@gmail.com>
2019-04-18 20:19:09 +0000

    Automatically backed up by Learn

---
+++ index0.js
@@ -0,0 +1,12 @@
+var recipes = {}
+
+//updateObjectWithKeyAndValue(object, key, value)
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return object.assign({}, obj, {[key]: value})
+}
+
+//destructivelyUpdateObjectWithKeyandValue(object, key, value) updates 'object' with the given 'key' and 'value'
+//return entire updated object
+function destructivelyUpdateObjectWithKeyandValue(object, key, value) {
+  
+}
