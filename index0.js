9607c222bd20dc2d9820bdb3904fb1bce26e0660
Adella <adellakenn@gmail.com>
2019-04-18 20:35:10 +0000

    Automatically backed up by Learn

--- index0.js
+++ index0.js
@@ -22,6 +22,6 @@ function deleteFromObjectByKey(object, key) {
 
 ///destructivelyDeleteFromObjectByKey(object, key) returns object without the dletekey/value pair
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return object
 }
